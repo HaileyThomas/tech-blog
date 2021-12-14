d2b973e6d273115d07ceed4279c495147decbca5
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-14 12:27:36 -0700

    update home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', {
+    id: 1,
+    title: 'Best Blog Ever',
+    post_content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    created_at: new Date(),
+    comment: [{}, {}],
+    user: {
+      username: 'test_user',
+    },
+  });
 });
 
 module.exports = router;
