2ba794ba7ec1eb625d9eb60d6c7669c7d86af5f5
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-13 19:30:00 -0700

    create and test update post routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -61,5 +61,31 @@ router.post('/', (req, res) => {
 });
 
 // update a post
+router.put('/:id', (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+      post_content: req.body.post_content,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// update a post
 
 module.exports = router;
