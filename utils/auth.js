0259d6d44073fe976c96cfcf04d7876007207b77
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-16 12:45:41 -0700

    add uthorization

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// check for existence of a session property and redirect
+const withAuth = (req, res, next) => {
+  if (!req.session.user_id) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
