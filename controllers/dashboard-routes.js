80ffa0328692cd145f2b95afdd21209d5ce31e25
Hailey Thomas <haileyraebeauty@gmail.com>
2021-12-16 13:25:46 -0700

    add delete post feature

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
@@ -30,6 +29,42 @@ router.get('/', withAuth, (req, res) => {
     })
     .catch((err) => {
       console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// EDIT POST
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: ['id', 'title', 'post_content', 'created_at'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render('edit-post', {
+          post,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
